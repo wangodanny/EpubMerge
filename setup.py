b386862d2727893e910226882a8e8cf973d66099
Jim Miller <retiefjimm@gmail.com>
2026-07-25 14:28:21 -0500

    Bump Test Version 3.2.3

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     name=package_name,
 
     # Versions should comply with PEP440.
-    version="3.2.2",
+    version="3.2.3",
     description='A tool for merging multiple epubs into one.',
     long_description=long_description,
 
